7ced9093d6dcb4050278ba072d080ee77d21776e
anxemer <anxemer224@gmail.com>
2023-08-16 00:32:14 +0700

    an

---
+++ QLBH_SOFTWARE/QLBH_SOFTWARE/KetNoi.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLBH_SOFTWARE
+{
+    internal class KetNoi
+    {
+        public string connectString = @"Data Source=LAPTOP-DRPTOLNJ\LENHU;Initial Catalog=Centrix;Integrated Security=True";
+        public SqlConnection getConnect()
+        {
+            SqlConnection sqlConn = new SqlConnection(connectString);
+            sqlConn.Open();
+            return sqlConn;
+        }
+    }
+}
